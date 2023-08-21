acf9294d1da05fb15f5ad89ecc71f66829865c5b
Hoàng Văn Tùng <tungvanzk03@gmail.com>
2023-08-21 21:41:19 +0700

    update

--- NodeJs/Express Server/index.js
+++ NodeJs/Express Server/index.js
@@ -2,6 +2,18 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Listening on ${port}`);
+app.get('/', (req, res) => {
+    res.send("<h1>Hello, world!</h1>");
 });
+
+app.get("/contact", (req, res) => {
+    res.send("<rem>Contact me</rem>");
+});
+
+app.get('/about', (req, res) => {
+    res.send("<h2>About me</h2>");
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
