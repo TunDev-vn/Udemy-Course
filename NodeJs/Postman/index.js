f3b7436cd612e907444b3521252830d004d72860
Hoàng Văn Tùng <tungvanzk03@gmail.com>
2023-08-22 08:35:08 +0700

    add morgan

---
+++ NodeJs/Postman/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello!</h1>");
+});
+
+app.post('/register', (req, res) => {
+    res.sendStatus(201);
+});
+
+app.put('/user/tung', (req, res) => {
+    res.sendStatus(200);
+});
+
+app.patch('/user/tung', (req, res) => {
+    res.sendStatus(200);
+});
+
+app.delete('/user/tung', (req, res) => {
+    res.sendStatus(200);
+});
+
+app.listen(port, () => {
+    console.log("Listening on port " + port);
+});
